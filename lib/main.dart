cb0038f91e2e980d2b5de789a9cdaba003a67460
sasansafari <sasansafari19@gmail.com>
2022-11-04 09:56:05 -0700

    HtmlEditorExample

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:tec/binding.dart';
 import 'package:tec/constant/my_colors.dart';
+import 'package:tec/view/articles/html_content_editor.dart';
 import 'package:tec/view/articles/manage_article.dart';
 import 'package:tec/view/articles/single.dart';
 import 'package:tec/view/articles/single_manage_article.dart';
@@ -55,7 +56,7 @@ class MyApp extends StatelessWidget {
               binding: ArticleManagerBinding()),
         ],
         // home: Single()
-        home: SplashScreen());
+        home: HtmlEditorExample(title: 'content',));
   }
 
   ThemeData lightTheme(TextTheme textTheme) {
